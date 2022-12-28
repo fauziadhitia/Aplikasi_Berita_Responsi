6d3ef4fd45ec9cfd523ba887e47201d912e540c2
fauziadhitia <fauzi.ihsa@students.utdi.ac.id>
2022-12-28 22:45:25 +0700

    Responsi

---
+++ app/src/test/java/com/androiddevs/mvvmnewsapp/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+package com.androiddevs.mvvmnewsapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+// Contoh tes unit lokal, yang akan dijalankan pada host
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
